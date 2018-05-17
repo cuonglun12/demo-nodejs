3aba042ffe0df84065abcf78d296add0e2f2e6aa
Ubuntu-64 <cuonglun12@gmail.com>
2018-05-17 15:21:08 +0700

    change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = restify.createServer();
 
 // Greeting endpoint.
 server.get('/hello/:name', function(req, res, next) {
-  res.send('hello ' + req.params.name);
+  res.send('How are you ' + req.params.name);
   next();
 });
 
